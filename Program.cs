53d50a3aaa07dad8b9170ac952323da7743926cc
yangryu321 <yangryu321@gmail.com>
2023-09-08 11:33:11 -0400

    Added in memory database

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 // Add services to the container.
 
 builder.Services.AddControllers();
